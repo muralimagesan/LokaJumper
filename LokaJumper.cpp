1d1fede058826bf3f92d0fb006ce5042cc03fce6
muralimagesan <muralimagesan@gmail.com>
2023-06-27 02:04:50 +1200

    Implement valid user input check

--- LokaJumper.cpp
+++ LokaJumper.cpp
@@ -84,8 +84,8 @@ void PrintGameStartupScreen()
 
     // Print welcome messages to the terminal
     std::cout << "You are a Cosmic Being stuck in Patala Loka and looking to find a way back to Mahakailasa..." << std::endl; //An example of Expression Statement
-    std::cout << "                      Using the initiation into the Universe of Numbers by SPH Sri Nithyananda Paramashivam," << std::endl;
-    std::cout << "                        Ascend to Mahakailasa by solving the combination challenge on each Loka ...\n" << std::endl;
+    std::cout << "                       Using the initiation into the Universe of Numbers by SPH Sri Nithyananda Paramashivam," << std::endl;
+    std::cout << "                         Ascend to Mahakailasa by solving the number combination challenge on each Loka...\n" << std::endl;
 }
 
 void PrintLokaIntro(int LokaIndex, string* LokaArray)
@@ -109,6 +109,34 @@ void PrintLokaIntro(int LokaIndex, string* LokaArray)
     }
 }
 
+bool isValidNumber(const std::string& input)
+{
+    std::istringstream iss(input);
+    double number;
+    char remaining;
+
+    // Attempt to extract a double from the input
+    if (!(iss >> number))
+    {
+        return false;
+    }
+
+    // Check if there is any remaining character after the number
+    if (iss >> remaining)
+    {
+        return false;
+    }
+
+    // Check if the number is within a valid range (optional)
+    if (number < 0.0)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+
 bool PlayGame(int LevelDifficulty)
 {
     // Declare 3 number code
@@ -124,11 +152,47 @@ bool PlayGame(int LevelDifficulty)
     std::cout <<"There are 3 numbers in this loka" << std::endl; //An example of Expression Statement
     std::cout <<"The numbers add-up to: " << CodeSum << std::endl;
     std::cout <<"The numbers multiply to give: " << CodeProduct << std::endl;
-    // std::cout <<"Enter the 3 numbers with a space in between each number, and when you are ready press Enter" << std::endl;
 
-    // Store player guess
-    int GuessA, GuessB, GuessC;
-    std::cin >> GuessA >> GuessB >> GuessC;
+    bool userInputValid;
+    string GuessA_S, GuessB_S, GuessC_S;            // Store player input
+    bool GuessA_IVN, GuessB_IVN, GuessC_IVN;        // Validate player input as a valid number
+    int GuessA, GuessB, GuessC;                     // Compare player answer with actual question
+
+    userInputValid = false;
+
+    while (!userInputValid)
+    {
+        std::cin >> GuessA_S >> GuessB_S >> GuessC_S;
+        GuessA_IVN = isValidNumber(GuessA_S);
+        GuessB_IVN = isValidNumber(GuessB_S);
+        GuessC_IVN = isValidNumber(GuessC_S);
+
+        if (GuessA_IVN && GuessB_IVN && GuessC_IVN)
+        {
+            GuessA = atoi(GuessA_S.c_str());
+            GuessB = atoi(GuessB_S.c_str());
+            GuessC = atoi(GuessC_S.c_str());
+            userInputValid = true;
+        }
+        else
+        {
+            std::cout << "\nOops...it looks like your input(s) were not valid numbers." << std::endl;
+            std::cout << "Please ensure you enter the 1st number followed with a single space, followed by the 2nd number with a single space and then the last number followed immediately with the 'Enter' key." << std::endl;
+            std::cout << "Below is an example of correct user input. Please note the '<--' is representing the 'Enter' key" << std::endl;
+            std::cout << "1 2 3<--\n" << std::endl;
+
+            std::cout << "The numbers add-up to: " << GuessA << std::endl;
+            std::cout << "The numbers add-up to: " << GuessB << std::endl;
+            std::cout << "The numbers add-up to: " << GuessC << std::endl;
+
+            std::cout << "Please enter the 3 numbers again to ascend from this loka" << std::endl;
+            std::cout << "The numbers add-up to: " << CodeSum << std::endl;
+            std::cout << "The numbers multiply to give: " << CodeProduct << std::endl;
+        }
+
+        //Ensures cin buffer is cleared before user inputs valid values again.
+        std::cin.clear();
+    }
 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
@@ -211,36 +275,6 @@ void PrintGameCompletionScreen()
 
                     )";
 }
-
-
-// bool isValidNumber(const std::string& input)
-// {
-//     std::istringstream iss(input);
-//     double number;
-//     char remaining;
-
-//     // Attempt to extract a double from the input
-//     if (!(iss >> number))
-//     {
-//         return false;
-//     }
-
-//     // Check if there is any remaining character after the number
-//     if (iss >> remaining)
-//     {
-//         return false;
-//     }
-
-//     // Check if the number is within a valid range (optional)
-//     // Modify the range as needed for your specific requirements
-//     // if (number < 0 || number > 100) {
-//     if (number < 0.0)
-//     {
-//         return false;
-//     }
-
-//     return true;
-// }
 
 
 // Main function introduces player to the story, initialises a few variables and prints out the sum and product to player.
